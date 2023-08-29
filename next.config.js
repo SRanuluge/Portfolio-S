d73010cbf2e7b1a147c672225826df9c0ac04ad3
Sanjeewa Ranuluge <sbandara725@gmail.com>
2023-08-30 02:22:01 +1200

    *

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "",
+  basePath: "/portfolio-s",
+  assetPrefix: "/portfolio-s",
 };
 
 module.exports = nextConfig;
