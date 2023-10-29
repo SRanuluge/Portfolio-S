122464fee6de74ce81f29b8eca65be56befe2ebb
Sanjeewa Ranuluge <sranuluge.dev@gmail.com>
2023-10-29 16:40:29 +1300

    feat: added contact form submition functionality

--- src/app/pages/contact/page.tsx
+++ src/app/pages/contact/page.tsx
@@ -16,13 +16,18 @@ const Contacts = () => {
       </section>
       <div className="mx-auto px-4 ">
         <section className=" relative max-w-4xl flex-1 py-10 px-4 -mt-[160px] mb-12  h-fit  mx-auto  w-full min-w-0 flex-col break-words rounded-sm bg-gray-50 dark:bg-gray-900 shadow-xl shadow-gray-500/5">
-          <form action="#" className=" space-y-8 ">
+          <form
+            action="https://getform.io/f/9de4233b-6cc3-4e02-a3ef-7c85e5e2a67a"
+            method="POST"
+            className=" space-y-8 "
+          >
             <div>
               <label className="block mb-2 text-sm font-medium">
                 Your email
               </label>
               <input
                 type="email"
+                name="email"
                 id="email"
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  "
                 placeholder="name@example.com"
@@ -33,6 +38,7 @@ const Contacts = () => {
               <label className="block mb-2 text-sm font-medium">Subject</label>
               <input
                 type="text"
+                name="subject"
                 id="subject"
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm  focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
                 placeholder="Let me know how i can help you"
@@ -45,6 +51,7 @@ const Contacts = () => {
               </label>
               <textarea
                 id="message"
+                name="message"
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
                 placeholder="Leave a comment..."
               ></textarea>
